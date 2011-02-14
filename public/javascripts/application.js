4cdc2cbd19225b21d4b8772bc4e02bab7216edf5
Szymon Urbaś <szymon.urbas@yahoo.com>
2011-02-14 11:04:28 +0100

    Filled in application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,107 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+// ---------------------------------------------------------------------
+//                              Utils
+// ---------------------------------------------------------------------
+var Utils = {
+	// 
+	// name: Utils.getElem
+	// @param i - id of element
+	// @return this is just for making work easier :D
+	getElem: function(i){
+		return document.getElementById(i);
+	},
+	
+	// 
+	// name: Utils.toggleElem
+	// @param toggleId - used to change '+' into '-' and vice versa
+	//        divId - the id of div that's gonna disappear
+	// @return changes the display of passed div
+	toggleElem: function(divId){
+		// If this is a box in bar
+		if (divId.indexOf('box') != -1){
+			var toggleId = divId.replace(/box/gi, 'toggle');
+			this.getElem(toggleId).innerHTML = (this.getElem(toggleId).innerHTML == '-') ? '+' : '-';
+			var cookie = Cookie.read('expanded_boxes');
+			var boxName = divId.replace('box:', '');
+			var oldValue = (this.getElem(divId).style.display == '') ? '1' : '0';
+			var newValue = (this.getElem(divId).style.display == '') ? '0' : '1'
+			var newCookieValue = cookie.replace(boxName + ":" + oldValue, boxName + ":" + newValue);
+			Cookie.new('expanded_boxes', newCookieValue, 365, '/')
+		}
+		
+		// Toggle the element
+		this.getElem(divId).style.display = (this.getElem(divId).style.display == '') ? 'none' : '';
+	},
+	
+	// 
+	// name: Utils.checkToggledElems
+	// @param
+	// @return toggles divs saved in cookies
+	checkToggledDivs: function(){
+		var cookie = Cookie.read('expanded_boxes');
+		elems = cookie.split('/');
+		
+		for (var i = 0; i < elems.length; i++){
+			if (elems[i].split(':')[1] == '0'){
+				this.toggleElem('box:' + elems[i].split(':')[0]);
+			}
+		}
+	},
+	
+	// 
+	// name: Utils.includeJS
+	// @param
+	// @return
+	includeJS: function(files){
+		for (var i = 0; i < files.length; i++){
+			document.write('<script type="text/javascript" src="/javascripts/' + files[i] + '.js"></script>');
+		}
+	},
+	
+	// 
+	// name: Utils.includeCSS
+	// @param 
+	// @return 
+	includeCSS: function(files){
+		for (var i = 0; i < files.length; i++){
+			document.write('<link href="/stylesheets/' + files[i] + '.css" rel="stylesheet" type="text/css" />');
+		}
+	}
+};
+
+// ---------------------------------------------------------------------
+//                              Cookies
+// ---------------------------------------------------------------------
+var Cookie = {
+	new: function(name, value, days, path){
+		var expiration = new Date();
+		expiration.setDate(expiration.getDate() + days);
+		document.cookie = name + "=" + escape(value) + 
+			((days) ? ";expires=" + expiration.toUTCString() : "") +
+			((path) ? ";path =" + path : '');
+	},
+	
+	read: function(name){
+		if (document.cookie.length > 0){
+			var start = document.cookie.indexOf(name + "=");
+			if (start != -1){
+				start = start + name.length + 1;
+				var end = document.cookie.indexOf(";", start);
+				if (end == -1) end = document.cookie.length;
+				return unescape(document.cookie.substring(start, end));
+			}
+		}
+		return "";
+	},
+	
+	destroy: function(name){
+		this.create(name, "", -1);
+	}
+};
+
+// Making source code cleaner
+var cssFiles = new Array('960_16', 'whitish', 'syntaxhighlighter/shCore', 'syntaxhighlighter/shThemeSemahawk');
+var jsFiles = new Array("jquery", "rails", "syntaxhighlighter/scripts/shCore", "syntaxhighlighter/scripts/shBrushRuby", "syntaxhighlighter/scripts/shBrushPython", "syntaxhighlighter/scripts/shBrushHTML", "syntaxhighlighter/scripts/shBrushCSS", "syntaxhighlighter/scripts/shBrushJScript");
+Utils.includeCSS(cssFiles);
+Utils.includeJS(jsFiles);
+
+document.onLoad = Utils.checkToggledDivs();
