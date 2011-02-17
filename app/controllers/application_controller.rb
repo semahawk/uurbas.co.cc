b0aadba5fc516ce15e03af2fc448129260991d41
Szymon Urbaś <szymon.urbas@yahoo.com>
2011-02-17 22:39:28 +0100

    Improved i18n a bit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,16 @@ class ApplicationController < ActionController::Base
 	end
 	
 	def set_locale
-		I18n.locale = params[:locale]
+		if params[:locale] == nil
+			I18n.locale = extract_locale_from_accept_language_header
+		else
+			I18n.locale = params[:locale]
+		end
 		logger.debug "* Locale set to '#{I18n.locale}'"
+	end
+	
+	private
+	def extract_locale_from_accept_language_header
+		request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
 	end
 end
