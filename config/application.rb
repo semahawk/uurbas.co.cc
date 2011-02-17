274c3cbd5ba4034703abfc0531b6c9bef10092af
Szymon Urbaś <szymon.urbas@yahoo.com>
2011-02-17 21:45:28 +0100

    Commented application.rb

--- config/application.rb
+++ config/application.rb
@@ -5,10 +5,10 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Home
 	class Application < Rails::Application
-		config.encoding = "utf-8"
-		config.filter_parameters += [:password]
+		config.encoding = "utf-8"	# Encoding
+		config.filter_parameters += [:password]	# Hey, web bandits, beware!
 		config.generators do |g|
-			g.template_engine :haml
+			g.template_engine :haml		# Converting ERB files to Haml ones, is boring ;)
 		end
 	end
 end
