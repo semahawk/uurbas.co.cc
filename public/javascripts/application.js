04970a10d717f36fdbac0c67d68c4590a7ff4bb4
Szymon Urbaś <urbas@hush.ai>
2012-05-03 15:16:49 +0200

    Funky animations on contact page.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,8 +2,38 @@ $(document).ready(function(){
 
   /// Share! ///
   $("#share img").mouseover(function(){
-    $("#share_h").html($(this).attr('alt'));
+    $("#share_h").html($(this).attr("alt"));
   }).mouseout(function(){
     $("#share_h").html("Share");
+  });
+
+  /// Contact buttons ///
+  $("ul#contact img").mouseover(function(){
+    var offset_top  = $(this).offset().top;
+    var offset_left = $(this).offset().left;
+
+    $("body").prepend("<div id='dummy' style='font-style: italic; color: #666; opacity: 0; position: fixed; top: " + (offset_top + 7) + "px; left: " + (offset_left + 40) + "px;'>" + $(this).attr("alt") + "</div>");
+
+    $("#dummy").animate({
+      opacity: 1,
+      left: '+=10'
+    }, {
+      duration: 450,
+      easing: 'easeOutBounce',
+      complete: function(){
+        // after animating
+      }
+    });
+  }).mouseout(function(){
+    $("#dummy").animate({
+      opacity: 0,
+      left: '-=10'
+    }, {
+      duration: 200,
+      easing: 'easeInSine',
+      complete: function(){
+        // after fading
+      }
+    });
   });
 });
